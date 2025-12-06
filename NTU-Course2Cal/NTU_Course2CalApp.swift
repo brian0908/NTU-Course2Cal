cb0c1b155d814f75b727c7086576c97c7cc8b958
Brian Lee <leebrian0908@gmail.com>
2025-12-06 22:32:56 +0800

    Initial Commit

---
+++ NTU-Course2Cal/NTU_Course2CalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTU_Course2CalApp.swift
+//  NTU-Course2Cal
+//
+//  Created by Brian Lee on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct NTU_Course2CalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
